61eb51cafad6aadb7494438a7c67a47b93a5bc55
VladNazarov <vladgitar99@gmail.com>
2020-04-04 11:37:53 +0300

    Added changes

--- 12/HomeWork/src/com/example/MyInteger.java
+++ 12/HomeWork/src/com/example/MyInteger.java
@@ -15,21 +15,10 @@ public class MyInteger implements Comparable<MyInteger> {
 		Integer count = 0;
 
 		for (int i = 2; i < this.data; i++) {
-			var isSimple = true;
 
 			if (data % i == 0) {
 
-				for (int j = 2; j < i; j++) {
-
-					if (i % j == 0) {
-						isSimple = false;
-						break;
-					}
-				}
-
-				if (isSimple) {
-					count++;
-				}
+				count++;
 			}
 		}
 		return count;
