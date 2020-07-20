fba87708b6e54f56917b8d50ff82e7074d8f2fe2
VladNazarov <vladgitar99@gmail.com>
2020-07-20 16:04:04 +0400

    Added homework

---
+++ 14/HomeWork/src/com/example/Detail.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+public class Detail {
+    private int id;
+
+    public Detail(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
